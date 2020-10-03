df682340bed50cca1ebb0c35498bdda5a1a26ef4
Alex M <alexmendanha@hotmail.com>
2020-10-02 22:43:18 -0300

    tentativa criação de js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function openNav () {
+
+    var x = document.getElementById("navigation");
+
+    if (x.className === "navigation") {
+        x.calssName += " menujs"; 
+    } else {
+        x.className = "navigation";
+    }
+
+
+
+
+}
