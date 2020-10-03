84dda1d9805982382d165dfc9c6b0a000902fa5b
Alex M <alexmendanha@hotmail.com>
2020-10-02 23:15:19 -0300

    finalização do site de portfdolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-function openNav () {
+
+function openNav() {
 
     var x = document.getElementById("navigation");
 
     if (x.className === "navigation") {
-        x.calssName += " menujs"; 
+        x.className += " menujs"; 
+        document.getElementById("threeline-icon").innerHTML = "&Cross";
     } else {
         x.className = "navigation";
+        document.getElementById("threeline-icon").innerHTML = "&#9776;";
     }
-
-
 
 
 }
